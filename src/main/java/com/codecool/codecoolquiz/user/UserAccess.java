b90f934e7ceee1eb7a31c61f6c5b51de0348c4e6
feshout <maciej.ukowski@gmail.com>
2018-05-21 12:01:09 +0200

    Add UserAccess model

---
+++ src/main/java/com/codecool/codecoolquiz/user/UserAccess.java
@@ -0,0 +1,44 @@
+package com.codecool.codecoolquiz.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Set;
+
+@Entity
+public class UserAccess {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+//    @JsonIgnore
+//    private Set<User> users;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public Set<User> getUsers() {
+//        return users;
+//    }
+//
+//    public void setUsers(Set<User> users) {
+//        this.users = users;
+//    }
+}
