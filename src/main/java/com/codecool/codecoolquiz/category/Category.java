01e098d9aa0d4aaab5046342f7797f829dcf2b0a
feshout <maciej.ukowski@gmail.com>
2018-05-21 13:34:29 +0200

    Add category model

--- src/main/java/com/codecool/codecoolquiz/category/Category.java
+++ src/main/java/com/codecool/codecoolquiz/category/Category.java
@@ -1,4 +1,92 @@
 package com.codecool.codecoolquiz.category;
 
+import com.codecool.codecoolquiz.question.Question;
+import com.codecool.codecoolquiz.user.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private boolean isActive=true;
+    @ManyToOne
+    private User createdBy;
+    private Date createDate;
+    @ManyToOne
+    private User modifyBy;
+    private Date modifyDate;
+    @JsonIgnore
+    @OneToMany
+    private Set<Question> questions;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public User getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(User modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public Set<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Set<Question> questions) {
+        this.questions = questions;
+    }
 }
