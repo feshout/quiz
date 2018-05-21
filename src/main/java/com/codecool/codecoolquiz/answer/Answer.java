3ab25dea4e2cb51c0907e4b26709bab7aaf73d51
feshout <maciej.ukowski@gmail.com>
2018-05-21 14:25:50 +0200

    Add fields isCorrect, createdBy, createDate, modifyBy and modifyDate, add setters and getters

--- src/main/java/com/codecool/codecoolquiz/answer/Answer.java
+++ src/main/java/com/codecool/codecoolquiz/answer/Answer.java
@@ -1,8 +1,10 @@
 package com.codecool.codecoolquiz.answer;
 
 import com.codecool.codecoolquiz.question.Question;
+import com.codecool.codecoolquiz.user.User;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Answer {
@@ -13,4 +15,75 @@ public class Answer {
     @ManyToOne
     @JoinColumn(name = "question_id")
     private Question question;
+    private Boolean isCorrect;
+    @ManyToOne
+    private User createdBy;
+    private Date createDate;
+    @ManyToOne
+    private User modifyBy;
+    private Date modifyDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public User getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(User modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public Boolean getCorrect() {
+        return isCorrect;
+    }
+
+    public void setCorrect(Boolean correct) {
+        isCorrect = correct;
+    }
 }
