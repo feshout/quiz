d5a060272879da8e829abf413026e922ad2181a2
feshout <maciej.ukowski@gmail.com>
2018-05-21 14:24:02 +0200

    Add Quiz model

---
+++ src/main/java/com/codecool/codecoolquiz/quiz/Quiz.java
@@ -0,0 +1,52 @@
+package com.codecool.codecoolquiz.quiz;
+
+import com.codecool.codecoolquiz.user.User;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date startDate;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User createBy;
+    @OneToMany
+    private Set<QuestionResponse> responses;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public User getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(User createBy) {
+        this.createBy = createBy;
+    }
+
+    public Set<QuestionResponse> getResponses() {
+        return responses;
+    }
+
+    public void setResponses(Set<QuestionResponse> responses) {
+        this.responses = responses;
+    }
+}
