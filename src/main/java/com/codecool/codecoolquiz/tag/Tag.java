c014c2c16508ffd43f12d68b4e00deaab10ee5d2
feshout <maciej.ukowski@gmail.com>
2018-05-21 14:26:16 +0200

    Add fields isCorrect, createdBy, createDate, modifyBy and modifyDate

--- src/main/java/com/codecool/codecoolquiz/tag/Tag.java
+++ src/main/java/com/codecool/codecoolquiz/tag/Tag.java
@@ -1,9 +1,11 @@
 package com.codecool.codecoolquiz.tag;
 
 import com.codecool.codecoolquiz.question.Question;
+import com.codecool.codecoolquiz.user.User;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.Date;
 import java.util.Set;
 
 @Entity
@@ -15,6 +17,12 @@ public class Tag {
     @JsonIgnore
     @ManyToMany
     private Set<Question> questions;
+    @ManyToOne
+    private User createdBy;
+    private Date createDate;
+    @ManyToOne
+    private User modifyBy;
+    private Date modifyDate;
 
     public Long getId() {
         return id;
@@ -32,4 +40,35 @@ public class Tag {
         this.questions = questions;
     }
 
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public User getModifyBy() {
+        return modifyBy;
+    }
+
+    public void setModifyBy(User modifyBy) {
+        this.modifyBy = modifyBy;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
 }
