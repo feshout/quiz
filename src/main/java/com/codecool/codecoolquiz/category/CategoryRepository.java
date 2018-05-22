0aaf75ba0c9cc47133dba2072322e983bdbbc308
Ernest <ernestjaroszek22@gmail.com>
2018-05-22 11:31:20 +0200

    add methods to search by Active field

--- src/main/java/com/codecool/codecoolquiz/category/CategoryRepository.java
+++ src/main/java/com/codecool/codecoolquiz/category/CategoryRepository.java
@@ -3,4 +3,10 @@ package com.codecool.codecoolquiz.category;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Iterable<Category> getAllByActiveTrue();
+
+    Category findByIdAndActiveTrue(long id);
+
+    Category findByNameAndActiveTrue(String name);
 }
