c6c73ab668aa0ab9f04745e4959c37877e51df6b
Ernest <ernestjaroszek22@gmail.com>
2018-05-22 11:33:36 +0200

    add Category Service methods implementation

---
+++ src/main/java/com/codecool/codecoolquiz/category/CategoryServiceImpl.java
@@ -0,0 +1,41 @@
+package com.codecool.codecoolquiz.category;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+     @Override
+    public Iterable<Category> getAll() {
+        return this.categoryRepository.getAllByActiveTrue();
+    }
+
+    @Override
+    public Category getById(long id) {
+        return this.categoryRepository.findByIdAndActiveTrue(id);
+    }
+
+    @Override
+    public Category getByName(String name) {
+        return this.categoryRepository.findByNameAndActiveTrue(name);
+    }
+
+    @Override
+    public void save(Category category) {
+        this.categoryRepository.save(category);
+    }
+
+    @Override
+    public void update(Category category) {
+        this.categoryRepository.save(category);
+    }
+
+    @Override
+    public void archiveById(long id) {
+        Category archivedCategory = this.categoryRepository.findByIdAndActiveTrue(id);
+        archivedCategory.setActive(false);
+        this.categoryRepository.save(archivedCategory);
+    }
+}
