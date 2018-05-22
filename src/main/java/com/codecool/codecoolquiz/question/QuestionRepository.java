24661dff1596545dd2e62429805740df463c0585
feshout <maciej.ukowski@gmail.com>
2018-05-22 13:11:20 +0200

    Add question repository with needed methods

--- src/main/java/com/codecool/codecoolquiz/question/QuestionRepository.java
+++ src/main/java/com/codecool/codecoolquiz/question/QuestionRepository.java
@@ -1,6 +1,20 @@
 package com.codecool.codecoolquiz.question;
 
+import com.codecool.codecoolquiz.category.Category;
+import com.codecool.codecoolquiz.tag.Tag;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface QuestionRepository extends CrudRepository<Question, Long> {
+
+    List<Question> getAllByIsActiveTrue();
+
+    List<Question> getAllByCategoryAndIsActiveTrue(Category category);
+//    Uncomment after implementing tag repository
+//    List<Question> getAllByTagAndIsActiveTrue(Tag tag);
+
+    Question getByTitleAndIsActiveTrue(String title);
 }
