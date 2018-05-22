0cd63af5ea75b795c970d2fe5415ab7aedc5be6f
feshout <maciej.ukowski@gmail.com>
2018-05-22 13:13:19 +0200

    Add question controller for cms

---
+++ src/main/java/com/codecool/codecoolquiz/question/QuestionController.java
@@ -0,0 +1,73 @@
+package com.codecool.codecoolquiz.question;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Controller
+@RequestMapping(path = "/questions")
+public class QuestionController {
+
+    private QuestionService questionService;
+
+    QuestionController(QuestionService questionService){
+        this.questionService = questionService;
+    }
+
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String getAll(Model model) {
+        model.addAttribute(questionService.getAll());
+        return "question/questions";
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public String getQuestionById(@PathVariable Long id, Model model) {
+
+        model.addAttribute(questionService.getById(id));
+        return "question/question";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.GET)
+    public String getQuestionForm(){
+        return "question/questionForm";
+    }
+
+    @RequestMapping(path = "/new", method = RequestMethod.POST)
+    public String addQuestion(@ModelAttribute Question question){
+        questionService.save(question);
+        Question newQuestion = questionService.getByTitle(question.getTitle());
+
+        return "redirect:question/question/" + newQuestion.getId().toString();
+    }
+
+    @RequestMapping(path = "/edit/{id}", method = RequestMethod.GET)
+    public String getQuestionFormToEdit(@PathVariable Long id, Model model){
+        model.addAttribute(questionService.getById(id));
+        return "question/questionForm";
+    }
+
+    @RequestMapping(path = "/edit/{id}", method = RequestMethod.PUT)
+    public String updateQuestion(@PathVariable Long id, @ModelAttribute Question question){
+        questionService.update(question);
+        return "redirect:question/" + id.toString();
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public String archiveQuestion(@PathVariable Long id) {
+        questionService.archiveById(id);
+        return "redirect:question/questions";
+    }
+
+
+
+
+
+
+
+}
