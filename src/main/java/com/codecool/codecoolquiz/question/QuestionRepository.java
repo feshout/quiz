528b6f42249805e6c6795e5be27d970aa3cbc8ad
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-23 13:42:13 +0200

    corrected imports

--- src/main/java/com/codecool/codecoolquiz/question/QuestionRepository.java
+++ src/main/java/com/codecool/codecoolquiz/question/QuestionRepository.java
@@ -1,6 +1,5 @@
 package com.codecool.codecoolquiz.question;
 
-import com.codecool.codecoolquiz.category.Category;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,5 +11,6 @@ public interface QuestionRepository extends CrudRepository<Question, Long> {
     @Query(
             "SELECT question FROM Question question WHERE category_id IN :categories"
     )
-    public List<Question> findByCategory(@Param("categories")List<Long> categories);
+    List<Question> findByCategory(@Param("categories")List<Long> categories);
+
 }
