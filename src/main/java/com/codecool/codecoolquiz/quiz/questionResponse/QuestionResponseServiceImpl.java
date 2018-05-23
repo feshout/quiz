af3736c52d6c04a23cd08b0b592a6a854d27db8f
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-23 13:40:22 +0200

    add QuestionResponseServiceImpl and implements method

---
+++ src/main/java/com/codecool/codecoolquiz/quiz/questionResponse/QuestionResponseServiceImpl.java
@@ -0,0 +1,25 @@
+package com.codecool.codecoolquiz.quiz.questionResponse;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionResponseServiceImpl implements QuestionResponseService {
+
+    QuestionResponseRepository repository;
+
+    public QuestionResponseServiceImpl(QuestionResponseRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public List<QuestionResponse> findAll() {
+        return null;
+    }
+
+    @Override
+    public QuestionResponse findByQuestionId(Long id) {
+        return repository.findByQuestionId(id);
+    }
+}
