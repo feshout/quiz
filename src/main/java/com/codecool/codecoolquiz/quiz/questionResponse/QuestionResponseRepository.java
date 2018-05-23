534400e3e0fb118f6e266f5d8b0f7a1bb76638fe
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-23 14:35:32 +0200

    remove death code change conception

--- src/main/java/com/codecool/codecoolquiz/quiz/questionResponse/QuestionResponseRepository.java
+++ src/main/java/com/codecool/codecoolquiz/quiz/questionResponse/QuestionResponseRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface QuestionResponseRepository  extends CrudRepository<QuestionResponse, Long> {
     QuestionResponse findByQuestionId(Long id);
-//    void update(QuestionResponse questionResponse); #in future
 }
