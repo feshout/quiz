ba32e9ca987fca0d7c2c58c4f3500ffa555e33fb
Przemo8922 <przemyslawsalak@gmail.com>
2018-05-23 13:35:26 +0200

    change QuestionResponseRepository and add findByQuestionId

--- src/main/java/com/codecool/codecoolquiz/quiz/questionResponse/QuestionResponseRepository.java
+++ src/main/java/com/codecool/codecoolquiz/quiz/questionResponse/QuestionResponseRepository.java
@@ -1,7 +1,7 @@
 package com.codecool.codecoolquiz.quiz.questionResponse;
 
-import com.codecool.codecoolquiz.quiz.questionResponse.QuestionResponse;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuestionResponseRepository  extends CrudRepository<QuestionResponse, Long> {
+    QuestionResponse findByQuestionId(Long id);
 }
