c69767a4d8e3a772e5c2ff236e59b8c672aea45e
Korges <korgesik@gmail.com>
2018-05-24 13:02:31 +0200

    Add new method to get single question from database

--- src/main/java/com/codecool/codecoolquiz/questionResponse/QuestionResponseRepository.java
+++ src/main/java/com/codecool/codecoolquiz/questionResponse/QuestionResponseRepository.java
@@ -1,7 +1,13 @@
 package com.codecool.codecoolquiz.questionResponse;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface QuestionResponseRepository  extends CrudRepository<QuestionResponse, Long> {
+
     QuestionResponse findByQuestionId(Long id);
+
+    @Query("SELECT response FROM QuestionResponse response WHERE response.answer IS NULL AND response.quiz = :quizId")
+    Iterable<QuestionResponse> findAllWhereAnswerIsNullAndQuizId(@Param("quizId") Long quizId);
 }
