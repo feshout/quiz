627af433b39c68bacff528d3c55c321cd1a92bc6
Pawel Rybka <pwl.rybka@gmail.com>
2018-06-05 10:44:36 +0200

    Create UserController class with getUserForm and addUser methods

---
+++ src/main/java/com/codecool/codecoolquiz/user/UserController.java
@@ -0,0 +1,42 @@
+package com.codecool.codecoolquiz.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+@RequestMapping(path = "/user")
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(path = "/new")
+    public String getUserForm() {
+        return "user/userForm";
+    }
+
+    @PostMapping(path = "/new")
+    public String addUser(@ModelAttribute User user) {
+
+        if(this.userService.getByLogin(user.getLogin()) == null) {
+
+            user.setDate(Timestamp.valueOf(LocalDateTime.now()));
+            userService.save(user);
+            return "login";
+        }
+        return "login";
+    }
+
+
+}
