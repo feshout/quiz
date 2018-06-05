2d5bf86daff397c0b0e8417eb5be0d21f07874f3
Pawel Rybka <pwl.rybka@gmail.com>
2018-06-05 10:34:12 +0200

    Add headers of getByLoginAndPassword, getByLogin methods and add annotation @Repository to UserRepository class

--- src/main/java/com/codecool/codecoolquiz/user/UserRepository.java
+++ src/main/java/com/codecool/codecoolquiz/user/UserRepository.java
@@ -1,6 +1,13 @@
 package com.codecool.codecoolquiz.user;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User getByLoginAndPassword(String login, String password);
+
+    User getByLogin(String login);
+
 }
