9f45a992e7775bbe8122611da30badf3661bf16a
Pawel Rybka <pwl.rybka@gmail.com>
2018-06-05 14:42:44 +0200

    Add Repository annotation to QuestionResponseRepository interface

--- src/main/java/com/codecool/codecoolquiz/questionResponse/QuestionResponseRepository.java
+++ src/main/java/com/codecool/codecoolquiz/questionResponse/QuestionResponseRepository.java
@@ -3,7 +3,9 @@ package com.codecool.codecoolquiz.questionResponse;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface QuestionResponseRepository  extends CrudRepository<QuestionResponse, Long> {
 
     QuestionResponse findByQuestionId(Long id);
