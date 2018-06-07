e638efc128b8148698ab72f8d75cdc4e84712d0b
Korges <korgesik@gmail.com>
2018-06-07 09:21:03 +0200

    Uncomment annotation, resolve conflict

--- src/main/java/com/codecool/codecoolquiz/questionResponse/QuestionResponseRepository.java
+++ src/main/java/com/codecool/codecoolquiz/questionResponse/QuestionResponseRepository.java
@@ -5,12 +5,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import java.util.List;
 
-=======
+
 @Repository
->>>>>>> origin/dev
+
 public interface QuestionResponseRepository  extends CrudRepository<QuestionResponse, Long> {
 
     QuestionResponse findByQuestionId(Long id);
